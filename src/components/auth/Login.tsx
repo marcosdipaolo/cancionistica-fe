24b41eb75c47e6cc559aa632f2b5cfe3bc2a726a
Marcos Di Paolo <marcosdipaolo@gmail.com>
2021-08-03 23:44:38 -0300

    wip

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -9,8 +9,8 @@ import Noti from "../messages/Noti";
 import SectionTitle from "../shared/SectionTitle";
 
 const Login: FC = () => {
-  const [ password, setPassword ] = useState('');
-  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState("");
+  const [ email, setEmail ] = useState("");
   const [ calling, setCalling ] = useState(false);
   const [ unauthorized, setUnauthorized ] = useState(false);
   const { dataStore: { userStore } } = useStore();
@@ -21,14 +21,12 @@ const Login: FC = () => {
     AuthService.login(
       { email, password }
     ).then((res: AxiosResponse) => {
-      setCalling(false);
       const { data }: { data: UserRegistrationResponse; } = res;
       userStore.login(data);
     }).catch(err => {
       setUnauthorized(true);
       setCalling(false);
     });
-
     setCalling(true);
   };
 
@@ -42,7 +40,7 @@ const Login: FC = () => {
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <SectionTitle title="Iniciá Sesión" />
+        <SectionTitle title="Iniciá Sesión"/>
         <div className="container">
           <div className="row">
             <div className="col-md-4 offset-md-4">
@@ -57,7 +55,7 @@ const Login: FC = () => {
               </div>
               <div className="form-group">
 
-              <input
+                <input
                   onChange={(e) => setPassword(e.target.value)}
                   value={password}
                   type="password"
@@ -66,17 +64,18 @@ const Login: FC = () => {
                 />
               </div>
               <div className="form-group">
-                <button type="submit" className="btn btn-primary d-block w-100 mt-5" style={{height: '45px'}} disabled={calling || unauthorized}>
-                  {calling ? (<span className="spinner-grow text-white m-0" role="status" aria-hidden="true" />) : ""}
+                <button type="submit" className="btn btn-primary d-block w-100 mt-5" style={{ height: "45px" }}
+                        disabled={calling || unauthorized}>
+                  {calling ? (<span className="spinner-grow text-white m-0" role="status" aria-hidden="true"/>) : ""}
                   {calling ? "" : <Fragment>Login</Fragment>}
                 </button>
               </div>
-              <Noti message="Credenciales no válidas" colorClass="danger" visible={alertVisible()} />
+              <Noti message="Credenciales no válidas" colorClass="danger" visible={alertVisible()}/>
             </div>
           </div>
         </div>
       </form>
-      {userStore.loggedUser ? <Redirect to="/" /> : ''}
+      {userStore.loggedUser ? <Redirect to="/"/> : ""}
     </div>
   );
 };
--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -7,20 +7,19 @@ export interface IAuthService {
   register: (data: UserRegisterRequest) => Promise<UserRegistrationResponse>;
   login: (data: UserLoginRequest) => Promise<any>;
 }
-const API_BASE_URL = "https://api-cancionistica.marcosdipaolo.com";
 
 export const AuthService: IAuthService = {
 
   register: async (data: UserRegisterRequest): Promise<UserRegistrationResponse> => {
     const { name, email, password, password_confirmation } = data;
-    await cancionistica.get(`${API_BASE_URL}/sanctum/csrf-cookie`);
-    return cancionistica.post(`${API_BASE_URL}/auth/register`, {
+    await cancionistica.get(`${process.env.REACT_APP_BACKEND_URL}/sanctum/csrf-cookie`);
+    return cancionistica.post(`${process.env.REACT_APP_BACKEND_URL}/auth/register`, {
       name, email, password, password_confirmation
     });
   },
 
   login: async (data: UserLoginRequest): Promise<any> => {
-    await cancionistica.get(`${API_BASE_URL}/sanctum/csrf-cookie`);
-    return cancionistica.post(`${API_BASE_URL}/auth/login`, data);
+    await cancionistica.get(`${process.env.REACT_APP_BACKEND_URL}/sanctum/csrf-cookie`);
+    return cancionistica.post(`${process.env.REACT_APP_BACKEND_URL}/auth/login`, data);
   }
 };
