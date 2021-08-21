bde18a49337c3d52ec8f7463eb551f8963063e67
Marcos Di Paolo <marcosdipaolo@gmal.com>
2021-08-21 14:02:44 -0300

    modules config touch

--- src/components/workshop/modules.config.ts
+++ src/components/workshop/modules.config.ts
@@ -1,5 +1,6 @@
 export interface WorkshopModuleData {
   index: number;
+  id: string,
   thumb: string;
   image: string;
   title: string;
@@ -10,32 +11,36 @@ export interface WorkshopModuleData {
 const config: WorkshopModuleData[] = [
   {
     index: 1,
-    image: '/images/cancionistica-1.jpg',
-    thumb: '/images/cancionistica-1.jpg',
+    id: "3aaa086a-a37b-434c-8336-8c99077d7a40",
+    image: '/images/cancionistica-3.jpg',
+    thumb: '/images/cancionistica-3.jpg',
     title: 'Modulo 1',
     subTitle: 'Subtítulo Modulo 1',
     content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dicta placeat numquam velit ex officia dolore quia inventore, tenetur provident sapiente quos quidem vitae repellat doloribus! Veritatis quasi quis eaque.'
   },
   {
     index: 2,
-    image: '/images/cancionistica-2.jpg',
-    thumb: '/images/cancionistica-2.jpg',
+    id: "d4266366-19c8-412e-802c-fd1bc71e02d7",
+    image: '/images/cancionistica-4.png',
+    thumb: '/images/cancionistica-4.png',
     title: 'Modulo 2',
     subTitle: 'Subtítulo Modulo 2',
     content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dicta placeat numquam velit ex officia dolore quia inventore, tenetur provident sapiente quos quidem vitae repellat doloribus! Veritatis quasi quis eaque.'
   },
   {
     index: 3,
-    image: '/images/cancionistica-3.jpg',
-    thumb: '/images/cancionistica-3.jpg',
+    id: "0977ba13-c697-498c-be21-591e05736b40",
+    image: '/images/cancionistica-5.jpg',
+    thumb: '/images/cancionistica-5.jpg',
     title: 'Modulo 3',
     subTitle: 'Subtítulo Modulo 3',
     content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dicta placeat numquam velit ex officia dolore quia inventore, tenetur provident sapiente quos quidem vitae repellat doloribus! Veritatis quasi quis eaque.'
   },
   {
     index: 4,
-    image: '/images/cancionistica-4.jpg',
-    thumb: '/images/cancionistica-4.jpg',
+    id: "b456c3bd-e7ba-4f9c-b4b7-370b18ee18fc",
+    image: '/images/cancionistica-6.jpg',
+    thumb: '/images/cancionistica-6.jpg',
     title: 'Modulo 4',
     subTitle: 'Subtítulo Modulo 4',
     content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dicta placeat numquam velit ex officia dolore quia inventore, tenetur provident sapiente quos quidem vitae repellat doloribus! Veritatis quasi quis eaque.'
